4486f9b208b99991a54f12a270ef7b00a5c90e7d
Nenkai <Nenkai@users.noreply.github.com>
2025-10-24 02:45:56 +0200

    serialization: forget System.Xml, use YAXLib

--- fftivc.utility.modloader/Tables/Serializers/XmlModelFormatSerializer.cs
+++ fftivc.utility.modloader/Tables/Serializers/XmlModelFormatSerializer.cs
@@ -4,7 +4,11 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Serialization;
+using System.Xml;
+// using System.Xml.Serialization; <- Not you, I hate you. not having support for not serializing nullable properties at all is CRIMINAL.
+
+using YAXLib; // <- YAXLib is my new homie
+using YAXLib.Enums;
 
 using fftivc.utility.modloader.Interfaces.Tables.Serializers;
 
@@ -14,44 +18,22 @@ public class XmlModelFormatSerializer : ITextModelSerializer
 {
     public T? Deserialize<T>(string content) where T : class
     {
-        try
-        {
-            var serializer = GetSerializer<T>();
-            using var reader = new StringReader(content);
-            return serializer.Deserialize(reader) as T;
-        }
-        catch
-        {
-            return null;
-        }
+        var serializer = GetSerializer<T>();
+        using var xmlReader = XmlReader.Create(content);
+        return serializer.Deserialize(xmlReader) as T;
     }
 
     public T? Deserialize<T>(Stream stream) where T : class
     {
-        try
-        {
-            var serializer = GetSerializer<T>();
-            return serializer.Deserialize(stream) as T;
-        }
-        catch
-        {
-            return null;
-        }
+        var serializer = GetSerializer<T>();
+        var xmlReader = XmlReader.Create(stream);
+        return serializer.Deserialize(xmlReader) as T;
     }
 
     public T? Deserialize<T>(ReadOnlySpan<byte> bytes) where T : class
     {
-        try
-        {
-            var serializer = GetSerializer<T>();
-
-            using var ms = new MemoryStream(bytes.ToArray()); // TODO: No alloc.
-            return serializer.Deserialize(ms) as T;
-        }
-        catch
-        {
-            return null;
-        }
+        var serializer = GetSerializer<T>();
+        return serializer.Deserialize(Encoding.UTF8.GetString(bytes)) as T; // XXX: Not the most efficient, I know.
     }
 
     public T? Deserialize<T>(ReadOnlyMemory<byte> bytes) where T : class
@@ -61,36 +43,29 @@ public class XmlModelFormatSerializer : ITextModelSerializer
 
     public string? Serialize<T>(T? model) where T : class
     {
-        try
-        {
-            var serializer = GetSerializer<T>();
+        YAXSerializer serializer = GetSerializer<T>();
 
-            using StringWriter textWriter = new StringWriter();
-            serializer.Serialize(textWriter, model);
-            return textWriter.ToString();
-        }
-        catch
-        {
-            return null;
-        }
+        using var stringWriter = new StringWriter();
+        using var xmlWriter = XmlWriter.Create(stringWriter, GetWriterSettings());
+        serializer.Serialize(model, xmlWriter);
+        return stringWriter.ToString();
     }
 
     public void Serialize<T>(Stream stream, T? model) where T : class
     {
-        try
-        {
-            var serializer = GetSerializer<T>();
+        var serializer = GetSerializer<T>();
+        var xmlWriter = XmlWriter.Create(stream, GetWriterSettings());
 
-            serializer.Serialize(stream, model);
-        }
-        catch
-        {
-            
-        }
+        serializer.Serialize(model, xmlWriter);
     }
 
-    private XmlSerializer GetSerializer<T>() where T : class
+    private static XmlWriterSettings GetWriterSettings() => new XmlWriterSettings { Indent = true };
+
+    private YAXSerializer GetSerializer<T>() where T : class
     {
-        return new XmlSerializer(typeof(T));
+        return new YAXSerializer(typeof(T), new YAXLib.Options.SerializerOptions()
+        {
+            SerializationOptions = YAXSerializationOptions.DontSerializeNullObjects, // :pray:
+        });
     }
 }
